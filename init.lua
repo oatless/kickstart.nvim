81eadb194448e334c357fef751f813ab16b3bcdd
oatless <minevovalev@gmail.com>
2024-05-31 19:09:47 +0100

    sharpest

--- init.lua
+++ init.lua
@@ -1021,6 +1021,7 @@ require('lazy').setup({
   },
 
   {'nvim-treesitter/nvim-treesitter'},
+  {'nvim-treesitter/nvim-treesitter-textobjects'},
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
